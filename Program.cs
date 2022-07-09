8096b45bbacab22dc9edac2a8b309c9601af3cfb
Vasiliy <vasiliyasazonov@gmail.com>
2022-07-09 17:06:25 +0300

    add task64

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Все задачи должны быть решены с помощью рекурсии!!!
+Задача 64: Task64
+Задача 66: Task66
+Задача 68: Task68
+
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+*/
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+NaturalNumber(number);
+
+int NaturalNumber(int number)
+{
+    if (number == 1)
+    {
+        Console.Write($"{number}. ");
+        return number;
+    }
+    else
+    {
+        Console.Write($"{number}, ");
+        NaturalNumber(number - 1);
+    }
+    return number;
+}
